b4ea040b57ecc16deed1b3619f9a73c6ff3e3279
vimalkumar <nairvimalkumar@gmail.com>
2021-04-03 16:48:09 +0530

    rollback server

--- backend/server.js
+++ backend/server.js
@@ -13,7 +13,6 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-mongodb+srv://VimalKumar:Mydatabase@cluster0.jwsdp.mongodb.net/myFirstDatabase?retryWrites=true&w=majority
 
 mongoose.connect(process.env.MONGODB_URL || 'mongodb://localhost/chocoT', {
   useNewUrlParser: true,
